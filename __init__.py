abdddc990a1d21f68af213f704999e5a25e07f04
Colin Talbert <talbertc@usgs.gov>
2014-02-07 07:15:26 -0700

    Incremented version number

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
+
+try:
+    from vistrails.core.configuration import ConfigurationObject
+except ImportError:
+    from core.configuration import ConfigurationObject
+
+
 name = "GeoDataPortal"
 identifier = "gov.usgs.GeoDataPortal"
-version = '0.0.1'
+version = '0.0.2'
+
+configuration = \
+    ConfigurationObject(cur_session_folder = r'C:\temp\SAHM_workspace',
+                        cur_WPS_URL = "default")
